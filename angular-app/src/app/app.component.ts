0630c0adf178c765fbbc0cf08431c3b17920b0f2
Logan Watanabe <loganwatanabe@gmail.com>
2020-04-29 14:09:38 -0400

    got basic apps and basic component firing

---
+++ angular-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import 'stencil-system-test-app'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-app';
+}
